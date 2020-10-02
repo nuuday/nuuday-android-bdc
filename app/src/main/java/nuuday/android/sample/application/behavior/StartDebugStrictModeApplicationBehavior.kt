406f297ad7bc12d686cafbaafa4d60e196c41a59
Andreas Rudolph <onemanmobile@gmail.com>
2020-10-02 15:40:40 +0200

    application sample

--- app/src/main/java/nuuday/android/sample/application/behavior/StartDebugStrictModeApplicationBehavior.kt
+++ app/src/main/java/nuuday/android/sample/application/behavior/StartDebugStrictModeApplicationBehavior.kt
@@ -1,2 +1,30 @@
 package nuuday.android.sample.application.behavior
 
+import android.os.StrictMode
+import androidx.databinding.library.BuildConfig
+import nuuday.android.sample.applications.behavior.ApplicationBehavior
+
+class StartDebugStrictModeApplicationBehavior : ApplicationBehavior {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyFlashScreen()
+                    .penaltyLog()
+                    .build()
+            )
+
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build()
+            )
+        }
+    }
+}
--- app/src/main/java/nuuday/android/sample/application/behavior/StartTimberApplicationBehavior.kt
+++ app/src/main/java/nuuday/android/sample/application/behavior/StartTimberApplicationBehavior.kt
@@ -1,2 +1,17 @@
 package nuuday.android.sample.application.behavior
 
+import nuuday.android.sample.BuildConfig
+import nuuday.android.sample.applications.behavior.ApplicationBehavior
+import timber.log.Timber
+
+class StartTimberApplicationBehavior: ApplicationBehavior {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+
+}
